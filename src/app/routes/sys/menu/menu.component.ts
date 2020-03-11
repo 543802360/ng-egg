6b3e09b7d2484e4729408c59ce9ab9eb7b1d3a88
543802360@qq.com <liushuo_whu@163.com>
2020-03-11 15:09:42 +0800

    添加菜单管理

--- src/app/routes/sys/menu/menu.component.ts
+++ src/app/routes/sys/menu/menu.component.ts
@@ -1,45 +1,100 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+/*
+ * @Author: your name
+ * @Date: 2020-03-10 16:24:46
+ * @LastEditTime: 2020-03-11 14:33:47
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /ng-egg/src/app/routes/sys/menu/menu.component.ts
+ */
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { _HttpClient, ModalHelper } from '@delon/theme';
 import { STColumn, STComponent } from '@delon/abc/table';
 import { SFSchema } from '@delon/form';
+import { SysMenuEditComponent } from './edit/edit.component';
+import { IMenu, array2tree } from '@shared';
 
 @Component({
-  selector: 'app-sys-menu',
+  selector
+    : 'app-sys-menu',
   templateUrl: './menu.component.html',
 })
-export class SysMenuComponent implements OnInit {
-  url = `/user`;
-  searchSchema: SFSchema = {
-    properties: {
-      no: {
-        type: 'string',
-        title: '编号'
-      }
-    }
-  };
-  @ViewChild('st', { static: false }) st: STComponent;
-  columns: STColumn[] = [
-    { title: '编号', index: 'no' },
-    { title: '调用次数', type: 'number', index: 'callNo' },
-    { title: '头像', type: 'img', width: '50px', index: 'avatar' },
-    { title: '时间', type: 'date', index: 'updatedAt' },
-    {
-      title: '',
-      buttons: [
-        // { text: '查看', click: (item: any) => `/form/${item.id}` },
-        // { text: '编辑', type: 'static', component: FormEditComponent, click: 'reload' },
-      ]
-    }
-  ];
+export class SysMenuComponent implements OnInit, AfterViewInit {
+
+  // 菜单数组数据
+  menuData: IMenu[];
+  // 菜单树节点数据
+  menuTreeData: IMenu[];
+  // 菜单折叠数据
+  mapOfMenuExpandedData: { [menu_id: string]: IMenu[] } = {};
 
   constructor(private http: _HttpClient, private modal: ModalHelper) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+
+  }
+  ngAfterViewInit() {
+    this.initMenus();
+  }
 
   add() {
     // this.modal
     //   .createStatic(FormEditComponent, { i: { id: 0 } })
     //   .subscribe(() => this.st.reload());
   }
+
+  initMenus() {
+    this.http.get('sys/menus').subscribe(resp => {
+      if (resp.success) {
+        this.menuData = resp.data;
+        this.menuTreeData = array2tree(this.menuData, 'menu_id', 'parent_id', 'children');
+
+        this.menuTreeData.forEach(item => {
+          this.mapOfMenuExpandedData[item.menu_id] = this.convertTreeToList(item);
+        });
+
+      } else {
+
+      }
+    });
+  };
+  collapse(array: IMenu[], data: IMenu, $event: boolean): void {
+    if ($event === false) {
+      if (data.children) {
+        data.children.forEach(d => {
+          const target = array.find(a => a.menu_id === d.menu_id)!;
+          target.expand = false;
+          this.collapse(array, target, false);
+        });
+      } else {
+        return;
+      }
+    }
+  }
+  convertTreeToList(root: IMenu): IMenu[] {
+    const stack: IMenu[] = [];
+    const array: IMenu[] = [];
+    const hashMap = {};
+    stack.push({ ...root, level: 0, expand: false });
+
+    while (stack.length !== 0) {
+      const node = stack.pop()!;
+      this.visitNode(node, hashMap, array);
+      if (node.children) {
+        for (let i = node.children.length - 1; i >= 0; i--) {
+          stack.push({ ...node.children[i], level: node.level! + 1, expand: false, parent: node });
+        }
+      }
+    }
+
+    return array;
+  }
+
+  visitNode(node: IMenu, hashMap: { [menu_id: string]: boolean }, array: IMenu[]): void {
+    if (!hashMap[node.menu_id]) {
+      hashMap[node.menu_id] = true;
+      array.push(node);
+    }
+  }
+
 
 }
