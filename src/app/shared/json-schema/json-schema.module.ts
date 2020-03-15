3e12e75c7a1f3725bb68c49699cdcd4130b731fc
543802360@qq.com <liushuo_whu@163.com>
2020-03-15 11:40:43 +0800

    添加font-awesome小部件（暂时有问题）

--- src/app/shared/json-schema/json-schema.module.ts
+++ src/app/shared/json-schema/json-schema.module.ts
@@ -1,13 +1,15 @@
+import { FontAwesomeWidget } from './widgets/fontawesome.widget';
 import { NgModule } from '@angular/core';
 import { DelonFormModule, WidgetRegistry } from '@delon/form';
 import { SharedModule } from '@shared';
-
+import { IconPickerModule } from "ngx-icon-picker";
 // import { TinymceWidget } from './widgets/tinymce/tinymce.widget';
 // import { UEditorWidget } from './widgets/ueditor/ueditor.widget';
 
 export const SCHEMA_THIRDS_COMPONENTS = [
   // TinymceWidget,
   // UEditorWidget
+  FontAwesomeWidget
 ];
 
 @NgModule({
@@ -15,6 +17,7 @@ export const SCHEMA_THIRDS_COMPONENTS = [
   entryComponents: SCHEMA_THIRDS_COMPONENTS,
   imports: [
     SharedModule,
+    IconPickerModule,
     DelonFormModule.forRoot()
   ],
   exports: [
@@ -23,7 +26,7 @@ export const SCHEMA_THIRDS_COMPONENTS = [
 })
 export class JsonSchemaModule {
   constructor(widgetRegistry: WidgetRegistry) {
-    // widgetRegistry.register(TinymceWidget.KEY, TinymceWidget);
+    widgetRegistry.register(FontAwesomeWidget.KEY, FontAwesomeWidget);
     // widgetRegistry.register(UEditorWidget.KEY, UEditorWidget);
   }
 }
