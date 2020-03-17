6a4320de7ab7f92e3d3ec75c6527cdbb417e359f
543802360@qq.com <liushuo_whu@163.com>
2020-03-17 15:15:11 +0800

    添加部门编辑组件

--- src/app/routes/sys/department/department.component.ts
+++ src/app/routes/sys/department/department.component.ts
@@ -1,45 +1,62 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { _HttpClient, ModalHelper } from '@delon/theme';
-import { STColumn, STComponent } from '@delon/abc/table';
-import { SFSchema } from '@delon/form';
+import { NzModalRef } from 'ng-zorro-antd/modal';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { _HttpClient } from '@delon/theme';
+import { SFSchema, SFUISchema } from '@delon/form';
 
 @Component({
   selector: 'app-sys-department',
   templateUrl: './department.component.html',
 })
 export class SysDepartmentComponent implements OnInit {
-  url = `/user`;
-  searchSchema: SFSchema = {
+  record: any = {};
+  i: any;
+  schema: SFSchema = {
     properties: {
-      no: {
-        type: 'string',
-        title: '编号'
-      }
-    }
+      no: { type: 'string', title: '编号' },
+      owner: { type: 'string', title: '姓名', maxLength: 15 },
+      callNo: { type: 'number', title: '调用次数' },
+      href: { type: 'string', title: '链接', format: 'uri' },
+      description: { type: 'string', title: '描述', maxLength: 140 },
+    },
+    required: ['owner', 'callNo', 'href', 'description'],
   };
-  @ViewChild('st', { static: false }) st: STComponent;
-  columns: STColumn[] = [
-    { title: '编号', index: 'no' },
-    { title: '调用次数', type: 'number', index: 'callNo' },
-    { title: '头像', type: 'img', width: '50px', index: 'avatar' },
-    { title: '时间', type: 'date', index: 'updatedAt' },
-    {
-      title: '',
-      buttons: [
-        // { text: '查看', click: (item: any) => `/form/${item.id}` },
-        // { text: '编辑', type: 'static', component: FormEditComponent, click: 'reload' },
-      ]
-    }
-  ];
+  ui: SFUISchema = {
+    '*': {
+      spanLabelFixed: 100,
+      grid: { span: 12 },
+    },
+    $no: {
+      widget: 'text'
+    },
+    $href: {
+      widget: 'string',
+    },
+    $description: {
+      widget: 'textarea',
+      grid: { span: 24 },
+    },
+  };
 
-  constructor(private http: _HttpClient, private modal: ModalHelper) { }
+  constructor(
+    private modal: NzModalRef,
+    private msgSrv: NzMessageService,
+    public http: _HttpClient,
+  ) {}
 
-  ngOnInit() { }
-
-  add() {
-    // this.modal
-    //   .createStatic(FormEditComponent, { i: { id: 0 } })
-    //   .subscribe(() => this.st.reload());
+  ngOnInit(): void {
+    if (this.record.id > 0)
+    this.http.get(`/user/${this.record.id}`).subscribe(res => (this.i = res));
   }
 
+  save(value: any) {
+    this.http.post(`/user/${this.record.id}`, value).subscribe(res => {
+      this.msgSrv.success('保存成功');
+      this.modal.close(true);
+    });
+  }
+
+  close() {
+    this.modal.destroy();
+  }
 }
