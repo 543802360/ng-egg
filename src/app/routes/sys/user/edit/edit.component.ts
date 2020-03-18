80112054299b5949e59b41fa75386c71684053d5
543802360@qq.com <liushuo_whu@163.com>
2020-03-18 22:57:39 +0800

    用户编辑、新建组件

--- src/app/routes/sys/user/edit/edit.component.ts
+++ src/app/routes/sys/user/edit/edit.component.ts
@@ -1,40 +1,111 @@
+import { IUser, array2tree, IDepartment } from '@shared';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NzModalRef } from 'ng-zorro-antd/modal';
 import { NzMessageService } from 'ng-zorro-antd/message';
 import { _HttpClient } from '@delon/theme';
 import { SFSchema, SFUISchema } from '@delon/form';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-sys-user-edit',
   templateUrl: './edit.component.html',
 })
 export class SysUserEditComponent implements OnInit {
-  record: any = {};
+  record: IUser = {};
   i: any;
   schema: SFSchema = {
     properties: {
-      no: { type: 'string', title: '编号' },
-      owner: { type: 'string', title: '姓名', maxLength: 15 },
-      callNo: { type: 'number', title: '调用次数' },
-      href: { type: 'string', title: '链接', format: 'uri' },
-      description: { type: 'string', title: '描述', maxLength: 140 },
+      // photo: {
+      //   type: 'string',
+      //   title: '头像'
+      // },
+      name: {
+        type: 'string',
+        title: '姓名',
+      },
+      username: {
+        type: 'string',
+        title: '用户名'
+      },
+      rolename: {
+        type: 'string',
+        title: '角色',
+      },
+      department_name: {
+        type: 'string',
+        title: '部门',
+      },
+      phone: {
+        type: 'string',
+        format: "mobile",
+        title: '手机',
+      },
+      email: {
+        type: 'string',
+        format: 'email',
+        title: '邮箱',
+      },
+      is_login: {
+        type: 'string',
+        title: '用户状态',
+        enum: [{ label: '正常', value: 'Y' }, { label: '禁用', value: 'N' }],
+        ui: {
+          widget: 'radio',
+
+        }
+      }
     },
-    required: ['owner', 'callNo', 'href', 'description'],
+    required: ['name', 'username', 'rolename', 'department_name'],
   };
   ui: SFUISchema = {
     '*': {
       spanLabelFixed: 100,
       grid: { span: 12 },
     },
-    $no: {
-      widget: 'text'
-    },
-    $href: {
+    $name: {
       widget: 'string',
+
     },
-    $description: {
-      widget: 'textarea',
-      grid: { span: 24 },
+    $username: {
+      widget: 'string',
+
+    },
+    // 角色，异步获取数据，通过pipe map操作符转换数据
+    $rolename: {
+      widget: 'select',
+      asyncData: () => {
+        return this.http.get('sys/roles').pipe(
+          map(resp => {
+            return resp.data.map(item => {
+              return {
+                label: item.rolename,
+                value: item.roleid
+              };
+            });
+          }))
+      }
+    },
+    // 部门，异步获取
+    $department_name: {
+      widget: 'tree-select',
+      asyncData: () => {
+        return this.http.get('sys/departments').pipe(
+          map(resp => {
+            const node = resp.data.map(item => {
+              return {
+                title: item.department_name,
+                key: item.department_id,
+                parent_id: item.parent_id,
+                parent_name: item.parent_name
+              };
+            });
+
+            return array2tree(node, 'key', 'parent_id', 'children');
+          }))
+      }
+    },
+    $phone: {
+      widget: 'string',
     },
   };
 
@@ -42,17 +113,32 @@ export class SysUserEditComponent implements OnInit {
     private modal: NzModalRef,
     private msgSrv: NzMessageService,
     public http: _HttpClient,
-  ) {}
+  ) { }
 
   ngOnInit(): void {
-    if (this.record.id > 0)
-    this.http.get(`/user/${this.record.id}`).subscribe(res => (this.i = res));
+    console.log('user edit ', this.record);
+    if (this.record.userid) {
+      this.http.get(`sys/user/${this.record.userid}`).subscribe(resp => {
+        if (resp.success) {
+          this.i = resp.data;
+        }
+      });
+    }
   }
 
-  save(value: any) {
-    this.http.post(`/user/${this.record.id}`, value).subscribe(res => {
-      this.msgSrv.success('保存成功');
+  save(user: IUser) {
+    const userEdited =
+      { ...user, ...{ roleid: user.rolename, rolename: null }, department_id: user.department_name, department_name: null };
+    this.http.post(`sys/user/update`, userEdited).subscribe(res => {
+      if (res.success) {
+        this.msgSrv.success(res.msg);
+      } else {
+        this.msgSrv.error(res.msg);
+
+      }
       this.modal.close(true);
+      // this.msgSrv.success('保存成功');
+      // this.modal.close(true);
     });
   }
 
