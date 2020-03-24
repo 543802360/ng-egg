ca35f95d188eab2007a05fb1f76b269ee4bf2c93
543802360@qq.com <liushuo_whu@163.com>
2020-03-24 20:56:26 +0800

    添加data在tab中显示标题

--- src/app/routes/exception/exception-routing.module.ts
+++ src/app/routes/exception/exception-routing.module.ts
@@ -7,14 +7,29 @@ import { Exception500Component } from './500.component';
 import { ExceptionTriggerComponent } from './trigger.component';
 
 const routes: Routes = [
-  { path: '403', component: Exception403Component },
-  { path: '404', component: Exception404Component },
-  { path: '500', component: Exception500Component },
-  { path: 'trigger', component: ExceptionTriggerComponent },
+  {
+    path: '403',
+    component: Exception403Component,
+    data: { title: '无权访问' }
+  },
+  {
+    path: '404',
+    component: Exception404Component,
+    data: { title: '未找到该页面' }
+  },
+  {
+    path: '500',
+    component: Exception500Component,
+    data: { title: '服务器返回错误' }
+  },
+  {
+    path: 'trigger',
+    component: ExceptionTriggerComponent
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class ExceptionRoutingModule {}
+export class ExceptionRoutingModule { }
