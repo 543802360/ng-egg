05458e7429c9403bf17d52896f80196ac1a14514
543802360@qq.com <liushuo_whu@163.com>
2020-03-27 23:23:58 +0800

    delete 部门管理

--- src/app/routes/sys/user/user.component.ts
+++ src/app/routes/sys/user/user.component.ts
@@ -5,7 +5,7 @@ import { STColumn, STComponent, STData, STChange, STColumnButton } from '@delon/
 import { SFSchema } from '@delon/form';
 import { IDepartment, IUser, array2tree, tree2array } from '@shared';
 import { NzMenuDirective, NzContextMenuService, NzFormatEmitEvent, NzModalService, NzTreeNode, NzMessageService, NzTreeComponent } from 'ng-zorro-antd';
-import { SysDepartmentComponent } from '../department/department.component';
+// import { SysDepartmentComponent } from '../log/department/department.component';
 import { map } from 'rxjs/operators';
 import { CacheService } from '@delon/cache';
 @Component({
@@ -20,9 +20,6 @@ export class SysUserComponent implements OnInit {
   departmentTreeNodes: any[] = [];
   // 右键选中treenode
   selectedNode: NzTreeNode;
-  // 拖拽
-  dragEnabled = false;
-  isDraged = false;
 
   // 用户表操作
   userData: IUser[];
@@ -222,79 +219,6 @@ export class SysUserComponent implements OnInit {
   }
 
   /**
-   * 添加部门节点
-   * @param root :是否为根节点
-   */
-  addDepartment(root?: string) {
-    const parent = root ? { parent_id: '', parent_name: '' } : { parent_id: this.selectedNode.key, parent_name: this.selectedNode.title };
-    this.modalSrv.create({
-      nzTitle: '编辑部门',
-      nzContent: SysDepartmentComponent,
-      nzComponentParams: {
-        record: parent
-      },
-      nzFooter: null
-    }).afterClose.subscribe(res => {
-      res ? this.initDepartmentTree() : null;
-    });
-
-  }
-
-  /**
-   * 编辑部门节点
-   */
-  editDepartment() {
-    const record = this.selectedNode.parentNode ?
-
-      {
-        parent_id: this.selectedNode.parentNode.key,
-        parent_name: this.selectedNode.parentNode.title,
-        department_id: this.selectedNode.key,
-        department_name: this.selectedNode.title
-      } :
-      {
-        parent_id: '',
-        parent_name: '',
-        department_id: this.selectedNode.key,
-        department_name: this.selectedNode.title
-      };
-    if (this.selectedNode) {
-      const modal = this.modalSrv.create({
-        nzTitle: '编辑部门',
-        nzContent: SysDepartmentComponent,
-        nzComponentParams: {
-          record
-        },
-        nzFooter: null
-      }).afterClose.subscribe(res => {
-        res ? this.initDepartmentTree() : null;
-      });
-    }
-  }
-
-  /**
-   * 删除部门
-   */
-  deleteDepartment() {
-    this.modalSrv.warning({
-      nzTitle: '提示',
-      nzContent: `确认删除【${this.selectedNode.title}】吗？该操作会删除部门下的所有用户，是否确认？`,
-      nzOnOk: () => {
-        this.http.delete(`sys/departments/${this.selectedNode.key}`).subscribe(resp => {
-          resp.success ?
-            this.msgSrv.success(resp.msg) : this.msgSrv.error(resp.msg);
-          setTimeout(() => {
-            this.initDepartmentTree();
-            this.initUsers();
-          });
-        });
-      },
-      nzCancelText: '取消',
-      nzOnCancel: () => { }
-    });
-  }
-
-  /**
    * 部门树节点单击
    * @param e
    */
@@ -315,59 +239,8 @@ export class SysUserComponent implements OnInit {
     this.contextSrv.create(e.event, menu);
     // this.departMenu.elementRef.nativeElement
   }
-  /**
-   * 拖拽完成事件
-   * @param e
-   */
-  departmentDragEnd(e) {
-    this.isDraged = true;
-  }
-  /**
-   * 刷新部门组织架构
-   */
-  refreshDepartment() {
-    this.initDepartmentTree();
-  }
-  /**
-   * 确认拖拽排序
-   */
-  confirmDrag() {
-    this.dragEnabled = !this.dragEnabled;
-    if (!this.isDraged) {
-      return;
-    }
-    // console.log(this.departmentTree.getTreeNodes());
-    // 拷贝部门树节点，并转换为扁平数组形式
-    const orderedNodes = tree2array(Object.values({ ...this.departmentTree.getTreeNodes() }), 'key', 'title', 'parentNode').map(item => {
-      return {
-        department_id: item.id,
-        department_name: item.name,
-        parent_id: item.parent_id,
-        parent_name: item.parent_name
-      }
-    });
-    this.http.post('sys/departments/updateAll', orderedNodes)
-      .pipe(map(resp => {
-        resp.success ? this.msgSrv.success(resp.msg) : this.msgSrv.error(resp.msg);
-      }))
-      .subscribe(resp => {
-        setTimeout(() => {
-          this.initDepartmentTree();
-        });
-      }, error => {
-        this.msgSrv.error(error);
-      });
-  }
-  /**
-   * 取消拖拽排序
-   */
-  cancelDrag() {
-    this.dragEnabled = !this.dragEnabled;
-    if (!this.isDraged) {
-      return;
-    }
-    this.initDepartmentTree();
-  }
+
+
   //#endregion
 
 
