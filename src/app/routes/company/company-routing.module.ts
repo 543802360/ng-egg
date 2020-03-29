c9d54938edf848986445ade51a4ea97319a3804c
543802360@qq.com <liushuo_whu@163.com>
2020-03-29 21:13:27 +0800

    添加业务子路由

--- src/app/routes/company/company-routing.module.ts
+++ src/app/routes/company/company-routing.module.ts
@@ -3,12 +3,51 @@ import { Routes, RouterModule } from '@angular/router';
 import { CompanyListComponent } from './list/list.component';
 import { CompanyPositionComponent } from './position/position.component';
 import { CompanyUsedNameComponent } from './used-name/used-name.component';
+import { ACLGuard } from '@delon/acl';
 
 const routes: Routes = [
+  {
+    path: '',
+    canActivateChild: [ACLGuard],
+    children:
+      [
+        {
+          path: 'list',
+          component: CompanyListComponent,
+          data: {
+            title: '企业名录管理',
+            guard: {
+              ability: ['/company/list'],
+            },
+            guard_url: 'exception/403'
+          }
+        },
+        {
+          path: 'position',
+          component: CompanyPositionComponent,
+          data: {
+            title: '企业位置管理',
+            guard: {
+              ability: ['/company/position'],
+            },
+            guard_url: 'exception/403'
+          }
+        },
+        {
+          path: 'used-name',
+          component: CompanyUsedNameComponent,
+          data: {
+            title: '企业曾用名管理',
+            guard: {
+              ability: ['/company/used-name'],
+            },
+            guard_url: 'exception/403'
+          }
+        }
+      ]
+  },
 
-  { path: 'list', component: CompanyListComponent },
-  { path: 'position', component: CompanyPositionComponent },
-  { path: 'used-name', component: CompanyUsedNameComponent }];
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
