aca33532cbb4024b065cfccb8b2fe99d42959a18
543802360@qq.com <liushuo_whu@163.com>
2020-04-01 11:46:32 +0800

    登记纳税人接口

---
+++ src/app/shared/models/IDjnsrxx.ts
@@ -0,0 +1,21 @@
+export interface IDjnsrxx {
+  DJXH?: string;
+  NSRMC?: string;
+  NSRSBH?: string;
+  SHXYDM?: string;
+  NSRZTMC?: string;
+  ZCDZ?: string;
+  LXR?: string;
+  LXDH?: string;
+  HYDM?: string;
+  HYMC?: string;
+  DJZCLXMC?: string;
+  DJZCLXDM?: string;
+  JDXZDM?: string;
+  JYFW?: string;
+  DJRQ?: Date;
+  LRRQ?: Date;
+  XGRQ?: Date;
+  Lat?: string;
+  Lng?: string;
+}
