8f297627e6a22d6c59cbe0d65a37b864d607828d
543802360@qq.com <liushuo_whu@163.com>
2020-04-15 22:12:52 +0800

    创建楼宇模型

--- src/app/routes/building-economic/create/create.component.ts
+++ src/app/routes/building-economic/create/create.component.ts
@@ -1,14 +1,50 @@
+import { LoadingService } from '@delon/abc';
 import { Component, OnInit } from '@angular/core';
-import { _HttpClient } from '@delon/theme';
+import { _HttpClient, ModalHelper } from '@delon/theme';
+import * as dark from "../../geo/styles/dark.json";
+import * as mapboxgl from "mapbox-gl";
+
 
 @Component({
   selector: 'app-building-economic-create',
   templateUrl: './create.component.html',
+  styleUrls: ['./create.component.less']
 })
 export class BuildingEconomicCreateComponent implements OnInit {
 
-  constructor(private http: _HttpClient) { }
+  style;
+  center;
+  zoom;
+  map: mapboxgl.Map;
 
-  ngOnInit() { }
+  mapboxDraw = require('@mapbox/mapbox-gl-draw');
+
+
+  constructor(private loadSrv: LoadingService) { }
+
+  ngOnInit() {
+    this.style = (dark as any).default;
+    this.loadSrv.open();
+
+    console.log('mapbox draw:', this.mapboxDraw);
+  }
+
+  add() {
+    // this.modal
+    //   .createStatic(FormEditComponent, { i: { id: 0 } })
+    //   .subscribe(() => this.st.reload());
+  }
+
+
+  mapboxglLoad(e) {
+    this.map = e;
+    this.loadSrv.close();
+    // const draw = new mapboxDraw();
+    // this.map.addControl(draw, 'top-left');
+  }
+
+  mapresize() {
+
+  }
 
 }
