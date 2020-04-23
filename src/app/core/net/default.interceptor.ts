1699b64da9559741390ae081203289521a410cc4
543802360@qq.com <liushuo_whu@163.com>
2020-04-23 12:51:08 +0800

    统一http拦截器，对200正常业务错误进行处理；以及http状态异常错误处理

--- src/app/core/net/default.interceptor.ts
+++ src/app/core/net/default.interceptor.ts
@@ -7,6 +7,7 @@ import {
   HttpErrorResponse,
   HttpEvent,
   HttpResponseBase,
+  HttpResponse,
 } from '@angular/common/http';
 import { Observable, of, throwError } from 'rxjs';
 import { mergeMap, catchError } from 'rxjs/operators';
@@ -38,7 +39,7 @@ const CODEMESSAGE = {
  */
 @Injectable()
 export class DefaultInterceptor implements HttpInterceptor {
-  constructor(private injector: Injector) { }
+  constructor(private injector: Injector, private msg: NzMessageService) { }
 
   private get notification(): NzNotificationService {
     return this.injector.get(NzNotificationService);
@@ -68,23 +69,24 @@ export class DefaultInterceptor implements HttpInterceptor {
       case 200:
         // 业务层级错误处理，以下是假定restful有一套统一输出格式（指不管成功与否都有相应的数据格式）情况下进行处理
         // 例如响应内容：
-        //  错误内容：{ status: 1, msg: '非法参数' }
-        //  正确内容：{ status: 0, response: {  } }
+        //  错误内容：{ success: false, msg: '非法参数' }
+        //  正确内容：{ success: true, response: {  } }
         // 则以下代码片断可直接适用
-        // if (event instanceof HttpResponse) {
-        //     const body: any = event.body;
-        //     if (body && body.status !== 0) {
-        //         this.msg.error(body.msg);
-        //         // 继续抛出错误中断后续所有 Pipe、subscribe 操作，因此：
-        //         // this.http.get('/').subscribe() 并不会触发
-        //         return throwError({});
-        //     } else {
-        //         // 重新修改 `body` 内容为 `response` 内容，对于绝大多数场景已经无须再关心业务状态码
-        //         return of(new HttpResponse(Object.assign(event, { body: body.response })));
-        //         // 或者依然保持完整的格式
-        //         return of(event);
-        //     }
-        // }
+        if (ev instanceof HttpResponse) {
+          const body: any = ev.body;
+          if (body && body.success === false) {
+            // this.msg.error(body.msg);
+            // 继续抛出错误中断后续所有 Pipe、subscribe 操作，因此：
+            // this.http.get('/').subscribe() 并不会触发
+            return throwError(body.msg);
+          } else {
+            this.msg.success(body.msg)
+            // 重新修改 `body` 内容为 `response` 内容，对于绝大多数场景已经无须再关心业务状态码
+            // return of(new HttpResponse({ ...ev, body: body.response }));
+            // 或者依然保持完整的格式
+            return of(ev);
+          }
+        }
         break;
       case 401:
         this.notification.error(`未登录或登录已过期，请重新登录。`, ``);
