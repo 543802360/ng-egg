e1259132214d42e79034d67a93e76ab542bace30
543802360@qq.com <liushuo_whu@163.com>
2020-08-02 23:23:00 +0800

    完善税收排名

--- src/app/routes/budget/bdg-analysis/company-order/company-order.component.ts
+++ src/app/routes/budget/bdg-analysis/company-order/company-order.component.ts
@@ -1,45 +1,185 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit, ChangeDetectorRef } from '@angular/core';
 import { _HttpClient, ModalHelper } from '@delon/theme';
-import { STColumn, STComponent } from '@delon/abc/st';
+import { STColumn, STComponent, STRes, STData, STPage, STChange } from '@delon/abc/st';
 import { SFSchema } from '@delon/form';
+import { NzMessageService } from 'ng-zorro-antd';
+import { CacheService } from '@delon/cache';
+import { yuan, IEOrder } from '@shared';
+import { getTimeDistance } from '@delon/util';
 
 @Component({
   selector: 'app-budget-bdg-analysis-company-order',
   templateUrl: './company-order.component.html',
+  styleUrls: ['./company-order.component.less']
 })
-export class BudgetBdgAnalysisCompanyOrderComponent implements OnInit {
-  url = `/user`;
-  searchSchema: SFSchema = {
-    properties: {
-      no: {
-        type: 'string',
-        title: '编号'
-      }
+export class BudgetBdgAnalysisCompanyOrderComponent implements OnInit, AfterViewInit {
+
+  // date
+  date_range: Date[] = [];
+  startDate: Date;
+  endDate: Date;
+  //#region 预算级次
+  budgetValue: number[] = [4]
+  budgetNodes = [{
+    title: '中央级',
+    value: 1,
+    key: 1
+  },
+  {
+    title: '市级',
+    value: 3,
+    key: 3
+  },
+  {
+    title: '区县级',
+    value: 4,
+    key: 4
+  }];
+
+  //#endregion
+  @ViewChild('st') st: STComponent;
+  url = "bdg/enterprise/order";
+  data: IEOrder[];
+  params = {
+    adminCode: 3302130000,
+    year: 2020,
+    startMonth: 1,
+    endMonth: 7,
+    bdg_value: this.budgetValue,
+    count: 100
+  };
+  total = 0;
+
+  res: STRes = {
+    process: (data: STData[], rawData?: any) => {
+      this.total = rawData.data.length;
+      return rawData.data;
     }
   };
-  @ViewChild('st') st: STComponent;
   columns: STColumn[] = [
-    { title: '编号', index: 'no' },
-    { title: '调用次数', type: 'number', index: 'callNo' },
-    { title: '头像', type: 'img', width: '50px', index: 'avatar' },
-    { title: '时间', type: 'date', index: 'updatedAt' },
     {
-      title: '',
-      buttons: [
-        // { text: '查看', click: (item: any) => `/form/${item.id}` },
-        // { text: '编辑', type: 'static', component: FormEditComponent, click: 'reload' },
-      ]
+      title: '排名',
+      width: 60,
+      className: 'text-center',
+      format: (item: STData, col: STColumn, index: number) => {
+        // console.log('index', index, item, col);
+        // console.log(this.st);
+        return `${(this.st.pi - 1) * this.st.ps + index + 1}`;
+
+      }
+
+    },
+    {
+      index: 'PAYER_ID',
+      title: '纳税人识别号',
+      className: 'text-center',
+
+    },
+    {
+      index: 'PAYMENT_NAME',
+      title: '纳税人名称',
+      className: 'text-center',
+
+    },
+    {
+      index: 'BNDSR',
+      title: '本年度收入',
+      className: 'text-center',
+      type: 'number'
+    },
+    {
+      index: 'SNTQ',
+      title: '上年同期',
+      className: 'text-center',
+      type: 'number'
+    },
+    {
+      index: 'TBZJZ',
+      title: '同比增减',
+      className: 'text-center',
+      // type: 'number'
+      render: 'tbzjz-tpl'
+    },
+    {
+      index: 'TBZJF',
+      title: '同比增减幅',
+      className: 'text-center',
+      render: 'tbzjf-tpl'
+
     }
-  ];
+  ]
 
-  constructor(private http: _HttpClient, private modal: ModalHelper) { }
-
-  ngOnInit() { }
-
-  add() {
-    // this.modal
-    //   .createStatic(FormEditComponent, { i: { id: 0 } })
-    //   .subscribe(() => this.st.reload());
+  page: STPage = {
+    show: true,
+    front: true,
+    pageSizes: [10, 20, 30, 50, 100]
   }
 
+  constructor(public http: _HttpClient,
+    public msg: NzMessageService,
+    private cdr: ChangeDetectorRef,
+    private cacheSrv: CacheService
+  ) {
+    const date = new Date();
+    this.startDate = new Date(date.getFullYear(), 0);
+    this.endDate = date;
+
+  }
+
+  ngOnInit() {
+
+  }
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.getData();
+    });
+  }
+
+  /**
+   * 设置日期
+   * @param type: 时间段类型
+   * @param flag
+   */
+  setDate(type: any) {
+    this.date_range = getTimeDistance(type);
+    this.startDate = this.date_range[0];
+    this.endDate = this.date_range[1];
+    setTimeout(() => this.cdr.detectChanges());
+  }
+
+
+  /**
+   * 获取税收统计概况（电子税票）
+   */
+  getData() {
+    this.budgetValue.length === 0 ? this.budgetValue = [4] : null;
+    this.http.get(this.url, this.getCondition()).subscribe(resp => {
+      this.data = resp.data;
+      this.total = resp.data.length;
+    })
+
+
+  }
+  /**
+   * 获取查询条件参数
+   */
+  getCondition() {
+    const year = this.startDate.getFullYear();
+    const startMonth = this.startDate.getMonth() + 1;
+    const endMonth = this.endDate.getMonth() + 1;
+    const budgetValue = this.budgetValue.toLocaleString();
+    const adminCode = '3302130000';
+
+    // const adminCode = this.cacheSrv.get('userInfo', { mode: 'none' }).department_id;
+
+    return { adminCode, year, startMonth, endMonth, budgetValue };
+  }
+  /**
+   * G2 PIE 图表tooltip
+   * @param value
+   */
+  change(e: STChange) {
+    console.log(e);
+  }
 }
