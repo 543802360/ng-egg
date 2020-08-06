8a6d8e53d2eb83ab99f1d36ea4b98af72874d16a
543802360@qq.com <liushuo_whu@163.com>
2020-08-06 10:59:55 +0800

    税收批量查询导出

--- src/app/routes/budget/bdg-analysis/batch-query/batch-query.component.ts
+++ src/app/routes/budget/bdg-analysis/batch-query/batch-query.component.ts
@@ -1,45 +1,135 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { ZSXM, EOrder, export2excel } from '@shared';
+import { Component, OnInit, ViewChild, ChangeDetectorRef } from '@angular/core';
 import { _HttpClient, ModalHelper } from '@delon/theme';
-import { STColumn, STComponent } from '@delon/abc/st';
+import { STColumn, STComponent, STPage } from '@delon/abc/st';
 import { SFSchema } from '@delon/form';
+import { XlsxService, LoadingService } from '@delon/abc';
+import { deepCopy } from '@delon/util';
+import { BdgSelectComponent } from 'src/app/shared/components/bdg-select/bdg-select.component';
+import { MonthRangeComponent } from 'src/app/shared/components/month-range/month-range.component';
+import { NzMessageService } from 'ng-zorro-antd';
 
 @Component({
   selector: 'app-budget-bdg-analysis-batch-query',
   templateUrl: './batch-query.component.html',
+  styleUrls: ['./batch-query.component.less']
 })
 export class BudgetBdgAnalysisBatchQueryComponent implements OnInit {
-  url = `/user`;
-  searchSchema: SFSchema = {
-    properties: {
-      no: {
-        type: 'string',
-        title: '编号'
-      }
-    }
-  };
-  @ViewChild('st') st: STComponent;
-  columns: STColumn[] = [
-    { title: '编号', index: 'no' },
-    { title: '调用次数', type: 'number', index: 'callNo' },
-    { title: '头像', type: 'img', width: '50px', index: 'avatar' },
-    { title: '时间', type: 'date', index: 'updatedAt' },
-    {
-      title: '',
-      buttons: [
-        // { text: '查看', click: (item: any) => `/form/${item.id}` },
-        // { text: '编辑', type: 'static', component: FormEditComponent, click: 'reload' },
-      ]
-    }
-  ];
+  url = `bdg/tools/batchQuery`;
+  xlsxColumns: STColumn[] = [];
+  xlsxData: any[] = [];
 
-  constructor(private http: _HttpClient, private modal: ModalHelper) { }
+  @ViewChild('bdgSelect') bdgSelect: BdgSelectComponent;
+  @ViewChild('monthRange') monthRange: MonthRangeComponent;
+
+  page: STPage = {
+    show: true,
+  }
+
+
+  matchNodes: any[] = [];
+  selectedMatchedField: string; @ViewChild('st') st: STComponent;
+
+
+  constructor(
+    private cdr: ChangeDetectorRef,
+    private http: _HttpClient,
+    private loadSrv: LoadingService,
+    private xlsx: XlsxService,
+    private msgSrv: NzMessageService) { }
 
   ngOnInit() { }
 
-  add() {
-    // this.modal
-    //   .createStatic(FormEditComponent, { i: { id: 0 } })
-    //   .subscribe(() => this.st.reload());
+
+  getCondition() {
+    const { startDate, endDate } = this.monthRange;
+    const year = startDate.getFullYear();
+    const startMonth = startDate.getMonth() + 1;
+    const endMonth = endDate.getMonth() + 1;
+    const budgetValue = this.bdgSelect.budgetValue.toLocaleString();
+
+    return { year, startMonth, endMonth, budgetValue };
+
+
   }
 
+  export() {
+
+    if (!this.selectedMatchedField) {
+      this.msgSrv.warning('请选择查询字段');
+      return;
+    }
+    if (!this.bdgSelect.budgetValue.length) {
+      this.msgSrv.warning('请选择预算级次');
+      return;
+    }
+
+    const nsrmcs = this.xlsxData.map(item => item[this.selectedMatchedField]);
+    this.http.post('bdg/tools/batchQuery', {
+      nsrmcs,
+      ...this.getCondition()
+    }).subscribe(resp => {
+
+      const rawData = resp.data.map(item => {
+        const el = {};
+        Object.keys(EOrder).forEach(key => {
+          el[EOrder[key]] = item[key];
+        });
+        Object.keys(ZSXM).forEach(key => {
+          el[ZSXM[key]] = item[key] ? item[key] : 0;
+        });
+
+        return el;
+
+      });
+
+      export2excel(`税收导出-${new Date().toLocaleString()}.xlsx`, '税收导出', rawData);
+
+    });
+  }
+
+  /**
+   * 读取excel表格数据
+   * @param e 
+   */
+  change(e) {
+    this.loadSrv.open({ text: '正在处理……' });
+    const node = e.target as HTMLInputElement;
+
+    this.xlsx.import(node.files[0]).then(res => {
+      // 默认获取表1为待匹配数据
+      const sheetNames = Object.keys(res);
+      const targetSheet = res[sheetNames[0]];
+      const columns = deepCopy(targetSheet[0]);
+      // 设置表头
+      this.xlsxColumns = columns.map(col => {
+        return {
+          index: col,
+          title: col,
+          className: 'text-center',
+        }
+      });
+      this.matchNodes = columns.map(col => {
+        return {
+          label: col,
+          value: col
+        }
+      });
+
+      setTimeout(() => {
+        this.st.resetColumns({ columns: this.xlsxColumns, emitReload: true })
+        // 获取待匹配的数据
+        targetSheet.shift();
+        this.xlsxData = targetSheet.map(value => {
+          const item = {};
+          columns.forEach((el, index) => {
+            item[el] = value[index] ? String(value[index]).trim() : 0;
+          });
+          return item;
+        });
+        this.loadSrv.close();
+
+      });
+    });
+  }
 }
