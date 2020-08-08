8be4c336ecf44e44c99180faf40334a81edf7ef0
543802360@qq.com <liushuo_whu@163.com>
2020-08-08 21:54:05 +0800

    创建纳税人名称提示组件

---
+++ src/app/shared/components/nsrmc-suggestion/nsrmc-suggestion.component.ts
@@ -0,0 +1,51 @@
+import { Subject } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { filter, debounceTime, switchMap } from 'rxjs/operators';
+import { _HttpClient } from '@delon/theme';
+
+@Component({
+  selector: 'app-nsrmc-suggestion',
+  templateUrl: './nsrmc-suggestion.component.html',
+  styles: [
+  ]
+})
+export class NsrmcSuggestionComponent implements OnInit {
+
+
+  _nsrmc: string;
+  autoChangeSub = new Subject<any>();
+  dataSource: string[];
+
+
+
+  public get nsrmc(): string {
+    return this._nsrmc;
+  }
+
+
+  constructor(private http: _HttpClient) { }
+
+  ngOnInit(): void {
+    // 搜索提示自动完成框
+    this.autoChangeSub
+      .pipe(
+        // filter(resp => {
+        //   return resp && resp.length >= 2
+        // }),
+        debounceTime(400),
+        switchMap(resp => {
+          return this.http.get('nsr/suggestion', { NSRMC: resp })
+        }))
+      .subscribe(resp => {
+
+        this.dataSource = resp.data;
+
+      });
+  }
+
+  setNsrmc(e) {
+    console.log('set nsrmc:', e);
+    this._nsrmc = e;
+  }
+
+}
