7dc20def4dc7927e772f201b58ab85a9d0e47c5e
543802360@qq.com <liushuo_whu@163.com>
2020-08-21 10:06:28 +0800

    镇街税收聚合图

--- src/app/routes/economic-analysis/map/tax-agg-map/tax-agg-map.component.ts
+++ src/app/routes/economic-analysis/map/tax-agg-map/tax-agg-map.component.ts
@@ -6,7 +6,7 @@ import { SFSchema } from '@delon/form';
 import { NzMessageService } from 'ng-zorro-antd';
 import * as mapboxgl from "mapbox-gl";
 import { dark } from "@geo";
-import { BdgSelectComponent, MonthRangeComponent } from '@shared';
+import { BdgSelectComponent, MonthRangeComponent, getColorRange } from '@shared';
 import { G2BarData } from '@delon/chart/bar';
 import { forkJoin } from 'rxjs';
 import { LoadingService } from '@delon/abc';
@@ -62,6 +62,26 @@ export class EconomicAnalysisMapTaxAggMapComponent implements OnInit, AfterViewI
     // }
   ];
 
+  colorStops =
+    [
+      "rgb(1, 152, 189)",
+      "rgb(73, 227, 206)",
+      "rgb(216, 254, 181)",
+      "rgb(254, 237, 177)",
+      "rgb(254, 173, 84)",
+      "rgb(209, 55, 78)"
+    ];
+  heightStop = 4000;
+  gridActive = {
+    type: "FeatureCollection",
+    features: []
+  };
+  empty = {
+    type: "FeatureCollection",
+    features: []
+  };
+  colorActive = "gold";
+
   constructor(public http: _HttpClient,
     private loadSrv: LoadingService,
     private modal: ModalHelper,
@@ -73,6 +93,9 @@ export class EconomicAnalysisMapTaxAggMapComponent implements OnInit, AfterViewI
     this.loadSrv.open({ text: '正在处理……' });
   }
 
+  /**
+   * 获取区域聚合数据
+   */
   getData() {
     const $townJson = this.http.get('assets/data/CY_TOWN.json');
     const $townTaxData = this.http.get(this.url, this.getCondition());
@@ -88,24 +111,130 @@ export class EconomicAnalysisMapTaxAggMapComponent implements OnInit, AfterViewI
         });
         // 2、获取Geometry
         const fc = resp[0];
-
+        const taxArray = [];
         fc.features.forEach(f => {
           const target = this.townData.find(i => i.jdxzmc === f.properties.MC);
           if (target) {
-            Object.defineProperty(f.properties, 'tax', {
-              value: target,
-              enumerable: true,
-              configurable: true
-            })
+            taxArray.push(target.bndsr);
+
+            Object.defineProperties(f.properties, {
+              'tax': {
+                value: target.bndsr,
+                enumerable: true,
+                configurable: true
+              },
+              'item': {
+                value: target,
+                enumerable: true,
+                configurable: true
+              }
+            });
           }
         });
+        const mintax = Math.min(...taxArray);
+        const maxtax = Math.max(...taxArray);
+        const colorRange = getColorRange(mintax, maxtax);
+        if (this.map.getSource('town-geo')) {
+          (this.map.getSource('town-geo') as mapboxgl.GeoJSONSource).setData(fc);
+        } else {
+          this.map.addSource('town-geo', {
+            type: 'geojson',
+            data: fc as any
+          });
+        }
+        // 判断是否存在高亮区域块数据源及图层
+        if (this.map.getLayer('grid-active')) {
+          this.map.removeLayer('grid-active');
+        }
+        if (this.map.getSource('grid-active')) {
+          this.map.removeSource('grid-active')
+        }
+        // 添加高亮数据源及图层
+        this.map.addSource("grid-active", {
+          type: "geojson",
+          data: this.gridActive as any
+        });
+        this.map.addLayer({
+          id: "grid-active",
+          type: "fill-extrusion",
+          source: "grid-active",
+          paint: {
+            "fill-extrusion-color": this.colorActive,
+            "fill-extrusion-opacity": 0.9,
+            "fill-extrusion-height-transition": {
+              duration: 1500
+            },
+            "fill-extrusion-color-transition": {
+              duration: 1500
+            }
+          }
+        });
+        if (!this.map.getLayer('town-layer')) {
+          this.map.addLayer({
+            id: 'town-layer',
+            source: 'town-geo',
+            type: 'fill-extrusion',
+            paint: {
+              "fill-extrusion-color": {
+                property: "tax",
+                stops: colorRange
+              },
+              "fill-extrusion-height": {
+                property: "tax",
+                stops: [[0, 0], [maxtax, this.heightStop]]
+              }, "fill-extrusion-opacity": 0.7,
+              "fill-extrusion-height-transition": {
+                duration: 1500
+              }
+            }
+          });
+        }
+        // mousemove
+        const popup = new mapboxgl.Popup({
+          closeButton: false,
+          closeOnClick: false,
+          anchor: "top"
+        });
+        this.map.on("mousemove", e => {
+          let html = "";
+          const coords = [e.lngLat.lng, e.lngLat.lat];
+          // console.log(e);
+          const queryPoint = this.map.queryRenderedFeatures(e.point, {
+            layers: ["town-layer"]
+          });
+          if (queryPoint.length) {
+            this.map.setPaintProperty("grid-active", "fill-extrusion-height", {
+              property: "tax",
+              stops: [[0, 0], [maxtax, this.heightStop]]
+            });
+            this.gridActive.features = [queryPoint[0]];
+            (this.map.getSource("grid-active") as any).setData(this.gridActive);
+            this.map.getCanvas().style.cursor = "pointer";
+            if (queryPoint[0].properties.item) {
+              const p = JSON.parse(queryPoint[0].properties.item);
+              html = `<h5>镇街：${p.jdxzmc}</h5>
+              <h5>本年度税收：${p.bndsr}&nbsp;万元</h5>
+              <h5>上年同期：${p.sntq}&nbsp;万元</h5>
+              <h5>同比增减幅：${p.tbzjf}</h5>`;
 
+              popup
+                .setLngLat(coords as any)
+                .setHTML(html)
+                .addTo(this.map);
+            }
+          } else {
+            // selectNsr = "";
+            (this.map.getSource("grid-active") as any).setData(this.empty);
+            this.map.getCanvas().style.cursor = "";
+            popup.remove();
+          }
+        });
 
       });
   }
 
   /**
-   * 
+   * 获取查询条件
    */
   getCondition() {
     this.bdgSelect.budgetValue.length === 0 ? this.bdgSelect.budgetValue = [4] : null;
@@ -118,12 +247,29 @@ export class EconomicAnalysisMapTaxAggMapComponent implements OnInit, AfterViewI
 
   }
 
+  /**
+   * mapbox load
+   * @param e 
+   */
   mapboxLoad(e) {
     this.loadSrv.close();
     this.map = e;
     setTimeout(() => {
       this.getData();
+      this.fly2target();
     });
+
+    (window as any).map = e;
+
   }
 
+  fly2target(center?, pitch?, zoom?, bearing?) {
+    this.map.flyTo({
+      center: center ? center : [120.33246, 36.276589],
+      zoom: zoom ? zoom : 9.688,
+      bearing: bearing ? bearing : 0,
+      pitch: pitch ? pitch : 46.5,
+      speed: 0.8
+    });
+  }
 }
