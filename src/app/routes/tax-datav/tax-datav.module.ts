7f55a2fc677187674863949d01a484970992a06b
543802360@qq.com <liushuo_whu@163.com>
2020-10-03 21:23:18 +0800

    创建税收概要组件

--- src/app/routes/tax-datav/tax-datav.module.ts
+++ src/app/routes/tax-datav/tax-datav.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '@shared';
 import { TaxDatavRoutingModule } from './tax-datav-routing.module';
 import { TaxDatavNavComponent } from './nav/nav.component';
+import { TaxDatavSummaryComponent } from './summary/summary.component';
 
 const COMPONENTS = [
-  TaxDatavNavComponent];
+  TaxDatavNavComponent,
+  TaxDatavSummaryComponent];
 const COMPONENTS_NOROUNT = [];
 
 @NgModule({
