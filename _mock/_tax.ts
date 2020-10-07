5cf474dcaa2a06702250bdf0ffb0da9f00639266
543802360@qq.com <liushuo_whu@163.com>
2020-10-07 23:32:13 +0800

    添加mock数据

---
+++ _mock/_tax.ts
@@ -0,0 +1,10 @@
+import { ssfjcData } from "./mock-data/ssfjc";
+import { zsxmData } from "./mock-data/zsxm";
+import { cyhyData } from "./mock-data/cyhy";
+
+export const TAXS = {
+    'POST /ssfjc': ssfjcData,
+    'POST /zsxm': zsxmData,
+    'POST /cyhy': cyhyData
+
+}
