d7b355b137df554d59744aa658702bc7396f4611
543802360@qq.com <liushuo_whu@163.com>
2021-01-07 22:43:37 +0800

    添加修改密码

--- src/app/routes/account/security/security.component.ts
+++ src/app/routes/account/security/security.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CacheService } from '@delon/cache';
 import { _HttpClient } from '@delon/theme';
+import { NzMessageService } from 'ng-zorro-antd/message';
 
 @Component({
   selector: 'app-account-security',
@@ -8,8 +10,28 @@ import { _HttpClient } from '@delon/theme';
 })
 export class AccountSecurityComponent implements OnInit {
   i: any = {};
-  constructor(private http: _HttpClient) { }
+  url = 'sys/user/updatepwd';
+  constructor(private http: _HttpClient,
+    private cacheSrv: CacheService,
+    private msgSrv: NzMessageService) { }
 
   ngOnInit() { }
+
+  ngSubmit() {
+    debugger;
+    if (this.i.pwd && this.i.repwd && this.i.pwd && this.i.repwd) {
+      const username = (this.cacheSrv.get('userInfo', { mode: 'none' })).username;
+      this.http.post(this.url, {
+        newPwd: this.i.pwd,
+        username
+      }).subscribe(resp => {
+        this.msgSrv.info(resp.msg)
+      })
+    } else {
+      this.msgSrv.warning('密码不一致，请核实！');
+    }
+
+
+  }
 
 }
